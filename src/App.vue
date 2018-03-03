8ecd9fe2662218fdcdb29b37fc59e1f2d257790e
皮娟 <trytry8090@163.com>
2018-03-03 15:11:46 +0800

    移除router&基本结构

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="app">
-    <router-view/>
+    <Main/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import Main from './pages/Main'
+
+  export default {
+    name: 'App',
+    components: {Main}
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/pages/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main">
+    <Page1/>
+  </div>
+</template>
+
+<script>
+  import Page1 from './page1/Page'
+
+  export default {
+    name: 'main',
+    components: {Page1}
+  }
+</script>
+
+<style scoped>
+
+</style>
